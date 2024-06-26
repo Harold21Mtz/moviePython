7235382e23f484273aef8d46d49b710c857cdfc8
HaroldMartinez <hfmartinezc@ufpso.edu.co>
2024-06-26 14:55:10 -0500

    ajuste

--- config/configDatabase.py
+++ config/configDatabase.py
@@ -3,17 +3,17 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-# user = os.getenv("DB_USER", "agro-web_owner")
-# password = os.getenv("DB_PASSWORD", "e7Dv9tYfojmp")
-# host = os.getenv("DB_HOST", "ep-cool-salad-a4dovsne.us-east-1.aws.neon.tech")
-# port = os.getenv("DB_PORT", "5432")
-# database = os.getenv("DB_NAME", "fastApi")
-
-user = os.getenv("DB_USER", "postgres")
-password = os.getenv("DB_PASSWORD", "Harold123")
-host = os.getenv("DB_HOST", "localhost")
+user = os.getenv("DB_USER", "agro-web_owner")
+password = os.getenv("DB_PASSWORD", "e7Dv9tYfojmp")
+host = os.getenv("DB_HOST", "ep-cool-salad-a4dovsne.us-east-1.aws.neon.tech")
 port = os.getenv("DB_PORT", "5432")
 database = os.getenv("DB_NAME", "fastApi")
+
+# user = os.getenv("DB_USER", "postgres")
+# password = os.getenv("DB_PASSWORD", "Harold123")
+# host = os.getenv("DB_HOST", "localhost")
+# port = os.getenv("DB_PORT", "5432")
+# database = os.getenv("DB_NAME", "fastApi")
 
 databaseUrl = f"postgresql://{user}:{password}@{host}:{port}/{database}"
 
